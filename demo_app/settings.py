c553c3c6fb93dae0164619e1effd131839431340
Nikoleta Zlateva <87802876+Nikoletazl@users.noreply.github.com>
2022-03-24 20:20:26 +0200

    working on the project

--- demo_app/settings.py
+++ demo_app/settings.py
@@ -11,10 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-import dj_database_url
-
-import os
-import psycopg2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -92,11 +88,6 @@ DATABASES = {
     }
 }
 
-DATABASE_URL = os.environ['DATABASE_URL']
-
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
